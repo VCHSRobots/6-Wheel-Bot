65d294619ce0c72e08e12b876b80fb02e1385413
Conner Taylor <conner.taylor8@gmail.com>
2018-11-06 20:25:20 -0800

    Add files via upload

---
+++ ecommons/RobotMap.java
@@ -0,0 +1,24 @@
+package ecommon;
+
+public class RobotMap {
+	
+	//Right Side Talon/Victor Numbers
+	public static int rMaster = 10;
+	public static int rMotor1 = 11;
+	public static int rMotor2 = 12;
+	//Left Side Talon/Victor Numbers
+	public static int lMotor1 = 13;
+	public static int lMotor2 = 14;
+	public static int lMaster = 15;
+	
+	//PDP Port Numbers
+	public static int rMasterCurrent = 5;
+	public static int rMotor1Current = 4;
+	public static int rMotor2Current = 3;
+	
+	public static int lMotor1Current = 2;
+	public static int lMotor2Current = 1;
+	public static int lMasterCurrent = 0;
+	
+
+}
